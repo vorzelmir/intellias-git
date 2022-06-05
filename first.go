41725c51e4a6e1cf9fd8522e0cb166391ddd180d
vorzelmir <vorzelmir@gmail.com>
2022-06-05 23:01:03 +0300

    add func second

--- first.go
+++ first.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func first() {
-  fmt.Println("Hello from first file")
+	fmt.Println("Hello from first file")
+}
+
+func second() {
+	fmt.Println("Hello from first file again")
 }
